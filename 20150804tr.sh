06d9af65de5656f35a49473bf7ceec8bcc104d4d
= <=>
2015-08-04 09:11:47 -0700

    20150804tr

---
+++ 20150804tr.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "this is test for command 'tr'"
+
+echo "1.79 1.83" > ./material/20150804tr_height.txt
+echo "1.65 1.59" >> ./material/20150804tr_height.txt
+
+echo "AbcDe" > ./material/20150804tr_word.txt
+
+echo "1234567890" > ./material/20150804tr_number.txt
+
+echo "file writing finished"
+########################################
+tr 1 2 < ./material/20150804tr_height.txt
+tr ' ' '\11' < ./material/20150804tr_height.txt
+tr 'a-z' 'A-Z' < ./material/20150804tr_word.txt
+tr '1-8' '[A*]'  < ./material/20150804tr_number.txt
+tr '1-8' '[A*5]B'  < ./material/20150804tr_number.txt
+
+
+
