8e2e38eea605a2dea8c8bce668fd5ec26ee2b974
= <=>
2015-08-04 18:50:29 -0700

    20150804tr judge folder's existence 02

--- 20150804tr.sh
+++ 20150804tr.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 echo "this is test for command 'tr'"
 
+
+########################################
+folder="./material"
+if [ ! -d "$folder" ];then
+	mkdir "$folder"
+fi
+ 
+
+
 echo "1.79 1.83" > ./material/20150804tr_height.txt
 echo "1.65 1.59" >> ./material/20150804tr_height.txt
 
@@ -13,7 +22,7 @@ echo "file writing finished"
 tr 1 2 < ./material/20150804tr_height.txt
 tr ' ' '\11' < ./material/20150804tr_height.txt
 tr 'a-z' 'A-Z' < ./material/20150804tr_word.txt
-tr '1-8' '[A*]'  < ./material/20150804tr_number.txt
+tr '1-8' '[**]'  < ./material/20150804tr_number.txt
 tr '1-8' '[A*5]B'  < ./material/20150804tr_number.txt
 
 
